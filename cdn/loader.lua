be18e125c87bfb9f75c054e57aafc8e89a13e701
Project-Rev-erse <kittypoptv@gmail.com>
2025-10-21 16:57:56 +0800

    Update loader.lua

--- cdn/loader.lua
+++ cdn/loader.lua
@@ -1,7 +1,16 @@
 ID = "32fc6b08-2829-4179-8e59-e807d04c35b1";
 
--- Load the Visual UI (it won't show yet)
-RUN_AFTER = loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"));
+-- This will run after your loader finishes joining the private server
+RUN_AFTER = function()
+    -- Step 1: Run your custom loading screen
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/Project-Rev-erse/0a66846f4bdd005f5059ce8d13782084/raw/8f7521de66e1985c8a1a53d62e22a6507e2b96a0/Project%2520Reverse%2520loading%2520ui", true))()
 
--- Load the main loader (it will call RUN_AFTER later)
+    -- Step 2: Wait 30 seconds (same as your loading screen)
+    task.wait(30)
+
+    -- Step 3: Load the visual UI after the loading screen
+    loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
+end
+
+-- Finally, run the loader (it’ll call RUN_AFTER once you’re in your private server)
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
