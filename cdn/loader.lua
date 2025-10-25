c95c3e484ba5b993c8792e8a416fa70365380daf
Project-Rev-erse <kittypoptv@gmail.com>
2025-10-25 20:26:00 +0800

    Update loader.lua

--- cdn/loader.lua
+++ cdn/loader.lua
@@ -1,16 +1,3 @@
-ID = "32fc6b08-2829-4179-8e59-e807d04c35b1";
-
--- This will run after your loader finishes joining the private server
-RUN_AFTER = function()
-    -- Step 1: Run your custom loading screen
-    loadstring(game:HttpGet("https://gist.githubusercontent.com/Project-Rev-erse/0a66846f4bdd005f5059ce8d13782084/raw/8f7521de66e1985c8a1a53d62e22a6507e2b96a0/Project%2520Reverse%2520loading%2520ui", true))()
-
-    -- Step 2: Wait 30 seconds (same as your loading screen)
-    task.wait(30)
-
-    -- Step 3: Load the visual UI after the loading screen
-    loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/visual.luau"))()
-end
-
--- Finally, run the loader (it’ll call RUN_AFTER once you’re in your private server)
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Steal-Tools-Hub-X/lumin-hub.lol/refs/heads/main/loader.lua",true))()
+‎
+‎loadstring(game:HttpGet("https://gist.githubusercontent.com/Project-Rev-erse/966512e53edbf890dfa199193efbacc5/raw/6521d9964600e02ed63bf07871dd8304de29ade0/Project%2520Reverse%2520ui.lua",true))()
