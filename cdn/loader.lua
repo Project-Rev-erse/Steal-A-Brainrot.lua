a8b59f6fc517613fbb294c4e6463e64ec8d75f28
Project-Rev-erse <kittypoptv@gmail.com>
2025-11-02 09:53:55 +0800

    Update loader.lua

--- cdn/loader.lua
+++ cdn/loader.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/WDZWI8tvDBdHDJKA/raw"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/LKjHv2vG", true))()
 loadstring(game:HttpGet("https://gist.githubusercontent.com/Project-Rev-erse/966512e53edbf890dfa199193efbacc5/raw/6521d9964600e02ed63bf07871dd8304de29ade0/Project%2520Reverse%2520ui.lua",true))()
