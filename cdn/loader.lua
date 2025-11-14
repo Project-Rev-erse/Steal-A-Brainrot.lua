3a45125ac63a1637d44277cc819e660710812bdf
Project-Rev-erse <kittypoptv@gmail.com>
2025-11-14 17:27:47 +0800

    Update loader.lua

--- cdn/loader.lua
+++ cdn/loader.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://pastebin.com/raw/LKjHv2vG", true))()
-loadstring(game:HttpGet("https://gist.githubusercontent.com/Project-Rev-erse/966512e53edbf890dfa199193efbacc5/raw/6521d9964600e02ed63bf07871dd8304de29ade0/Project%2520Reverse%2520ui.lua",true))()
+
